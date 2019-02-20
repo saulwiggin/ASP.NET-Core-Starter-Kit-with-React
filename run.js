6c2f88d52b5d233dbeaa353304db31624bb20bd8
DrSaulWiggin+1-at-359151331797 <saulwiggin@googlemail.com>
2019-02-20 18:18:52 +0000

    add all

--- run.js
+++ run.js
@@ -110,7 +110,7 @@ tasks.set('publish', () => {
   global.DEBUG = process.argv.includes('--debug') || false;
   const remote = {
     name: 'azure',
-    url: 'https://<user>@<app>.scm.azurewebsites.net:443/<app>.git', // TODO: Update deployment URL
+    url: 'https://paymentsapp.scm.azurewebsites.net:443/paymentsapp.git', // TODO: Update deployment URL
   };
   const opts = { cwd: path.resolve(__dirname, './build'), stdio: ['ignore', 'inherit', 'inherit'] };
   const git = (...args) => new Promise((resolve, reject) => {
